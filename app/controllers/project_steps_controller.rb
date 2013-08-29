6e22f914d58201017d5d3c1b769b9cd1e2e1fd71
Jonas Didzbalis <jonas@didzbalis.com>
2013-08-29 15:03:59 -0700

    Improvements made to Project Submission pages

--- app/controllers/project_steps_controller.rb
+++ app/controllers/project_steps_controller.rb
@@ -1,6 +1,6 @@
 class ProjectStepsController < ApplicationController
 	include Wicked::Wizard
-	steps :add_facility, :add_energyuse, :add_projectinfo, :add_audit
+	steps :add_energyuse, :add_projectinfo, :add_audit
 
 	def show
 		@project = Project.find(params[:project_id])
@@ -11,6 +11,11 @@ class ProjectStepsController < ApplicationController
 		@project = Project.find(params[:project_id])
 		@project.update_attributes(params[:project])
 		render_wizard @project
+	end
+
+	def finish_wizard_path
+  		flash[:info] = "Thanks for submitting your project. You can track the status of all your projects here."
+  		projects_path
 	end
 
 
